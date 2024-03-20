5af295c7a8ac496ac1ee0ab0396340ca880a2b66
Anton Beletsky <AntonBeletskyEU@gmail.com>
2024-03-20 10:45:11 +0200

    1

---
+++ i1/my-react-app/src/App.js
@@ -0,0 +1,40 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  var v1 = (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+
+      <div className="trulala">table</div>
+      <table>
+        <tr>1</tr>
+        <tr>1</tr>
+        <tr>
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+        </tr>
+      </table>
+    </div>
+  );
+
+  return v1;
+}
+
+export default App;
---
+++ i1/my-react-app/src/Kurwa.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function Kurwa() {
+  return (
+    <div className="Kurwa">
+      <header className="Kurwa-header">KURWA</header>
+    </div>
+  );
+}
+
+export default Kurwa;
